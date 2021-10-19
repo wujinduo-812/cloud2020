ed17d5018adf0eaa7094c716617979981e86e975
武金鐸 <wujinduo@sinosoft.hk>
2021-10-19 15:08:20 +0800

    IDEA整合GitHub測試

---
+++ cloud-gatewat-gateway9527/src/main/java/com/atguigu/springcloud/GateWayMain9527.java
@@ -0,0 +1,13 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringCloudApplication
+@EnableEurekaClient
+public class GateWayMain9527 {
+    public static void main(String[] args) {
+        SpringApplication.run( GateWayMain9527.class,args);
+    }
+}
